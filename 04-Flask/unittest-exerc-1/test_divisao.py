25966484e9abee8985787518fca8634bf67349c4
moisesfdasilva <msesfernandes@gmail.com>
2024-02-15 23:06:47 -0300

    feat: create more seven folders in 04-Flask.

---
+++ 04-Flask/unittest-exerc-1/test_divisao.py
@@ -0,0 +1,16 @@
+import unittest
+
+from divisao import divide
+
+
+class TestOperations(unittest.TestCase):
+    def test_divide(self):
+        self.assertEqual(divide(5, 2), 2.5)
+
+    def test_divide_by_0(self):
+        with self.assertRaises(ZeroDivisionError):
+            divide(5, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
