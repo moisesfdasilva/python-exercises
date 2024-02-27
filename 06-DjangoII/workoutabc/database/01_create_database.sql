0244e6a0e7e5d72226db948017f6f6cbb02b9197
moisesfdasilva <msesfernandes@gmail.com>
2024-02-27 14:06:12 -0300

    feat: create project workoutabc in 06-DajngoII.

---
+++ 06-DjangoII/workoutabc/database/01_create_database.sql
@@ -0,0 +1,3 @@
+CREATE DATABASE IF NOT EXISTS workoutabc_database;
+
+USE workoutabc_database;
---
+++ 06-DjangoII/workoutabc/workoutapp/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        # Permite acesso se o objeto pertence a quem faz a requisição
+        return obj.workout_plan.personal_trainer == request.user
