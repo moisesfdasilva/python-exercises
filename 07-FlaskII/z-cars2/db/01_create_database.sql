ee28fa342435d689ea2dddddc61d2db3def6f4f3
moisesfdasilva <msesfernandes@gmail.com>
2024-03-13 22:42:13 -0300

    feat: create file 01_create_database.sql in zcars2/db.

---
+++ 07-FlaskII/z-cars2/db/01_create_database.sql
@@ -0,0 +1,19 @@
+CREATE DATABASE IF NOT EXISTS z_cars_database;
+
+USE z_cars_database;
+
+CREATE TABLE cars (
+    id integer not null auto_increment,
+    brand varchar(100),
+    model varchar(100),
+    year integer,
+    PRIMARY KEY (id)
+);
+
+INSERT INTO cars (brand, model, year)
+VALUES
+('Mitsubishi', 'Pajero', 2011),
+('Volkswagen', 'Amarok', 2020),
+('Dodge', 'Ram', 2018),
+('Ford', 'Ranger', 2017),
+('Chevrolet', 'S10', 2020);
